e62333448b81b814460e26c5262b11b0d7f9a009
MysteriousBits <80115356+MysteriousBits@users.noreply.github.com>
2022-07-07 23:02:33 +0600

    adding it

---
+++ ExtremelyDangerousRansomware!.bat
@@ -0,0 +1,12 @@
+@echo off
+echo Encrypting data...
+timeout 5 > nul
+echo Done! Successfully encrypted all of your datas.
+echo To decrypt and get everything back, pay $500000 at http://cruelRansomwares.lol/coronaXD/pay
+echo Or hit any key to destroy your pc! lol
+pause > nul
+taskkill /f /im explorer.exe > nul
+timeout 5 > nul
+start explorer.exe > nul
+echo XD XD. I can see your horrified face! XD
+pause > nul
