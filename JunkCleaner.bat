88a58e9b6f264108e9d9c8811eb3bac04baf02b5
MysteriousBits <80115356+MysteriousBits@users.noreply.github.com>
2025-05-05 07:04:07 +0600

    FIxed recycle bin clearing method, added thumbnails and delivery optimization files to the  cleaning list

--- JunkCleaner.bat
+++ JunkCleaner.bat
@@ -1,20 +1,53 @@
 @echo off
-echo All files in recyclebin, user temp, windows temp directories will be deleted.
+title Full Windows Trash Cleaner
+
+echo This will permanently delete:
+echo - Recycle Bin
+echo - Temp files (User + System)
+echo - Prefetch
+echo - Windows Update cache
+echo - Thumbnail cache
+echo - Delivery optimization files
+echo - Windows upgrade folders ($WINDOWS.~BT / $WINDOWS.~WS)
+echo.
 pause
+
 echo Cleaning...
-rd /s /q %SYSTEMDRIVE%\$Recycle.bin >nul 2>&1
-cd /D %TEMP%
+
+:: Recycle Bin (all users)
+powershell -command "Get-ChildItem 'C:\$Recycle.Bin' -Force -Recurse | Remove-Item -Force -Recurse" >nul 2>&1
+powershell -command "Clear-RecycleBin -Force" >nul 2>&1
+
+:: User Temp
+cd /D "%TEMP%"
 for /d %%D in (*) do rd /s /q "%%D" >nul 2>&1
 del /f /q * >nul 2>&1
-cd %SYSTEMDRIVE%\Windows\Temp >nul 2>&1
+
+:: Windows Temp
+cd /D "%SYSTEMROOT%\Temp"
 for /d %%D in (*) do rd /s /q "%%D" >nul 2>&1
 del /f /q * >nul 2>&1
-cd %SYSTEMDRIVE%\Windows\Prefetch >nul 2>&1
+
+:: Prefetch
+cd /D "%SYSTEMROOT%\Prefetch"
+del /f /q * >nul 2>&1
+
+:: Windows Update Cache
+cd /D "%SYSTEMROOT%\SoftwareDistribution\Download"
 for /d %%D in (*) do rd /s /q "%%D" >nul 2>&1
 del /f /q * >nul 2>&1
-cd %SYSTEMDRIVE%\Windows\SoftwareDistribution\Download >nul 2>&1
-for /d %%D in (*) do rd /s /q "%%D" >nul 2>&1
+
+:: Delivery Optimization
+cd /D "%ProgramData%\Microsoft\Network\Downloader"
 del /f /q * >nul 2>&1
-echo Done. Deleted all accessible junk files.
-echo Hit any key to exit.
+
+:: Thumbnail Cache (current user)
+del /f /q "%LOCALAPPDATA%\Microsoft\Windows\Explorer\thumbcache_*.db" >nul 2>&1
+
+:: Windows Upgrade Folders
+rd /s /q "%SystemDrive%\$WINDOWS.~BT" >nul 2>&1
+rd /s /q "%SystemDrive%\$WINDOWS.~WS" >nul 2>&1
+
+echo Done. All accessible junk files have been deleted.
+echo Press any key to exit.
 pause >nul
